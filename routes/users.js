cfe15be4a78849aa4549aa58e3bd70cfa56b1e29
peifeng1115 <peifeng1115@gmail.com>
2019-05-03 11:54:32 +0800

    加入swagger

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,14 @@ const crud = require('../mysql/sql');
 
 const error = require('../mysql/error');
 
-/** 取得列表 */
+/** 
+ * @swagger
+ * /api/users:
+ *    get:
+ *       tags:
+ *       - User
+ *       summary: 取得列表
+ */
 router.get('/', async (req, res) => {
   try {
 
@@ -67,70 +74,5 @@ router.delete('/:id', async (req, res) => {
     return error.serverError(e);
   }
 });
-
-
-// /* GET users List */
-// router.get('/', function (req, res, next) {
-//   connection.query('SELECT * FROM User', (error, rows) => {
-//     if (error) {
-//       res.status(500).send(error);
-//       return;
-//     }
-//     res.send(rows);
-//   });
-// });
-
-// /* GET users by id */
-// router.get('/:id', function (req, res, next) {
-//   connection.query('SELECT * FROM User WHERE Id = ?', req.params.id, (error, rows) => {
-//     if (error) {
-//       res.status(500).send(error);
-//       return;
-//     }
-//     if (!rows.length) {
-//       res.status(404).send('Not Found');
-//       return;
-//     }
-//     res.send(rows[0]);
-//   });
-// });
-
-// /* POST users create */
-// router.post('/', (req, res, next) => {
-//   var sql = {
-//     name: req.body.name
-//   };
-//   connection.query('INSERT INTO User SET ?', sql, (error, rows) => {
-//     if (error) {
-//       res.status(500).send(error);
-//       return;
-//     }
-//     res.send(rows);
-//   });
-// });
-
-// /* PUT users update */
-// router.put('/', (req, res, next) => {
-//   var id = req.body.id;
-//   var item = req.body;
-//   connection.query('UPDATE User SET ? WHERE id = ?', [item, id], (error, rows) => {
-//     if (error) {
-//       res.status(500).send(error);
-//       return;
-//     }
-//     res.send(rows);
-//   });
-// });
-
-// /* DELETE users by id */
-// router.delete('/:id', (req, res, next) => {
-//   connection.query('DELETE FROM User WHERE id = ?', req.params.id, (error, rows) => {
-//     if (error) {
-//       res.status(500).send(error);
-//       return;
-//     }
-//     res.send(rows);
-//   });
-// });
 
 module.exports = router;
